9ea7b12010aa53309c8b9d531090682131a638f9
Ritik052500 <132008550+Ritik052500@users.noreply.github.com>
2023-05-31 14:49:48 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package demo;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Trying webhook");
+		System.out.println(System.getProperty("choice"));
 	}
 }
